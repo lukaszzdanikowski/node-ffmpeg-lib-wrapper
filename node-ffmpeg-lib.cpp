7a564390dfa6239960ee510c6617245e340541f9
£ukasz Zdanikowski <lukasz.zdanikowski@gmail.com>
2016-03-18 11:48:30 +0100

    remove pdf test

--- node-ffmpeg-lib.cpp
+++ node-ffmpeg-lib.cpp
@@ -5,10 +5,6 @@ void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 }
 
 void Init(v8::Local<v8::Object> exports) {
-    base::FilePath module_path;
-    if (!PathService::Get(base::DIR_MODULE, &module_path))
-      return false;
-    base::FilePath::StringType name(FILE_PATH_LITERAL("pdf.dll"));
   exports->Set(Nan::New("hello").ToLocalChecked(),
                Nan::New<v8::FunctionTemplate>(Method)->GetFunction());
 }
